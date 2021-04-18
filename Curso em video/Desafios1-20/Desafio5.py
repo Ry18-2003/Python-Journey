a06cfb13b2a7dc32656916a947d45e07bd34f37b
Richard Chrispe <82769873+Ry18-2003@users.noreply.github.com>
2021-04-18 14:12:00 -0300

    Desafios 5, 6 e 7

---
+++ Curso em video/Desafios1-20/Desafio5.py
@@ -0,0 +1,2 @@
+n1 = int(input('Digite um número: '))
+print(f'O antecessor do número {n1} é {n1-1} e seu sussesor é {n1+1}.')
---
+++ Curso em video/Desafios1-20/Desafio6.py
@@ -0,0 +1,2 @@
+n1 = int(input('Digite um número: '))
+print(f'O dobro do número {n1} é {n1*2} o seu triplo {n1*3} e sua raiz quadrada é {n1**(1/2)}')
